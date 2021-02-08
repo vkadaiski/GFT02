f86af24c51b33f3bba90d41b1da4ab5f1cc5b6ed
vkadaiski <vkadaiski@gmail.com>
2021-02-08 10:57:54 -0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+/*
+Considerando os conceitos de Orientação a Objetos, crie uma classe abstrata Pai de nome Pessoa, contendo os atributos (nome, rg).
+ 
+Crie uma Classe Filha chamada Funcionario, contendo os atributos (salario_inicial, percentual e salario_total) encapsulados, 
+com seus respectivos seletores (getters) e modificadores (setters).
+Criar uma classe principal que instancie o objeto Funcionario com as regras abaixo: 
+Um funcionário de uma empresa recebe aumento salarial anualmente.Sabe - se que:
+Esse funcionário foi contratado em 2016, com salário inicial de R$ 1.000,00. Em 2017, ele recebeu aumento de 1,5 % sobre seu salário inicial.
+A partir de 2018(inclusive), os aumentos salariais sempre corresponderam ao dobro do percentual do ano anterior. 
+Faça um programa que informe o Nome, RG e o salário em 2020 desse funcionário.
+*/
+
+
+namespace ex02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        abstract void Pessoas(){
+
+            
+
+        }
+
+    }
+}
